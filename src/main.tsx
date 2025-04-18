733d1471601fef3df29690301da5e7f075b02c94
Amr <amrabohassab123@gmail.com>
2025-04-19 00:17:33 +0200

    problem in version part 3 delete dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,6 @@ import App from "./App.tsx";
 import AuthProvider from "./Context/AuthContext/AuthContext.tsx";
 import { ToastContainer } from "react-toastify";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "./../node_modules/@tanstack/react-query-devtools/src/index";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
@@ -21,7 +20,6 @@ createRoot(document.getElementById("root")!).render(
         <ToastContainer />
         <App />
       </AuthProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 );
